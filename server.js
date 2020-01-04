3f7d102ad51f7a5d6268c3c1ab583a02b1feca52
Dan Usem <danusem@gmail.com>
2020-01-04 16:46:57 -0600

    Project 2 express app setup

---
+++ server.js
@@ -0,0 +1,31 @@
+// Require modules
+const express = require('express');
+const port = 3000; // We'll eventually set the port dynamically
+const logger = require('morgan');
+const indexRouter = require('./routes/index');
+
+
+// Set up express app
+const app = express();
+
+// Connect to DB
+
+require('./config/database');
+
+// Configure Express App app.set()
+app.set('view engine', 'ejs');
+
+// Mount middleware app.use()
+app.use(logger('dev'));
+app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// Mount Routes app.use()
+
+app.use('/', indexRouter);
+
+// Tell App to listen
+app.listen(port, () => {
+    console.log(`Express is listening on port:${port}`);
+});
